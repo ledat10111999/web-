34360daa4536f5bb463e327d03b0522e34355f0b
ledat10111999 <56331527+ledat10111999@users.noreply.github.com>
2020-05-30 13:23:24 +0700

    new

--- routes/signin.js
+++ routes/signin.js
@@ -6,32 +6,37 @@ router.get('/signin', function (req, res, next) {
     res.render('SignInPage', { data: {} });
 });
 router.post('/signin', function (req, res, next) {
-    var param = req.body;
+    if(!req.session.user){
+        var param = req.body;
 
-    if (param.username.trim().length == 0) {
-        res.render('SignInPage', { data: { error: "Bạn chưa nhập tài khoản" } });
-    }
-    if (param.password.trim().length == 0) {
-        res.render('SignInPage', { data: { error: "Bạn chưa nhập Mật khẩu" } });
-    } else {
-        var data = user_check.checkU(param.username);
-        if (data) {   //nếu có data thì tiến hành
-            data.then(function (users) {  // lấy dữ liệu trong data
-                var user = users[0]; // lấy mảng dữ liệu user thứ 0
-                var status = hash.check_password(param.password, user.Pass); // hàm này trả về true hoặc false, true khi param.password == user.Pass, false thì ngược lại
-                if (status == false) { // nếu status trả về false tức param.password !=user.Pass
-                    res.render('SignInPage', { data: { error: "Tài khoản hoặc mật khẩu sai" } });// gửi lên SignInPage object data
-                } else { // nếu không == true thì thông báo đăng nhập thành công 
-                    req.session.user = user; // lưu thông tin biến user vào session
-                    res.redirect('/');
-                }
-            }).catch(function (err) {
-                res.render('SignInPage', { data: { error: "Tài khoản hoặc mật khẩu sai " } });
-            })
-        } else {
-            res.render('SignInPage', { data: { error: "Tài khoản hoặc mật khẩu sai" } });
+        if (param.username.trim().length == 0) {
+            res.render('SignInPage', { data: { error: "Bạn chưa nhập tài khoản" } });
         }
+        if (param.password.trim().length == 0) {
+            res.render('SignInPage', { data: { error: "Bạn chưa nhập Mật khẩu" } });
+        } else {
+            var data = user_check.checkU(param.username);
+            if (data) {   //nếu có data thì tiến hành
+                data.then(function (users) {  // lấy dữ liệu trong data
+                    var user = users[0]; // lấy mảng dữ liệu user thứ 0
+                    var status = hash.check_password(param.password, user.Pass); // hàm này trả về true hoặc false, true khi param.password == user.Pass, false thì ngược lại
+                    if (status == false) { // nếu status trả về false tức param.password !=user.Pass
+                        res.render('SignInPage', { data: { error: "Tài khoản hoặc mật khẩu sai" } });// gửi lên SignInPage object data
+                    } else { // nếu không == true thì thông báo đăng nhập thành công 
+                        req.session.user = user; // lưu thông tin biến user vào session
+                        res.redirect('/');
+                    }
+                }).catch(function (err) {
+                    res.render('SignInPage', { data: { error: "Tài khoản hoặc mật khẩu sai " } });
+                })
+            } else {
+                res.render('SignInPage', { data: { error: "Tài khoản hoặc mật khẩu sai" } });
+            }
+        }
+    }else{
+        res.redirect(req.originalUrl);
     }
+   
 })
 module.exports = router;
 
--- routes/signup.js
+++ routes/signup.js
@@ -7,64 +7,69 @@ router.get('/signup', function (req, res, next) { // khi user /signup thì dẫn
   res.render('signup', { data: {} }); // render đến thư mục /router/signup.ejs, data là 1 object gửi đến đường dẫn
 });
 router.post('/signup', function (req, res) {
-  var user = req.body; // lấy dữ liệu từ body
+  if(!req.session.user){
+    var user = req.body; // lấy dữ liệu từ body
 
-  if (user.lastname.trim().length == 0) // username là name = "username" trong form 
-  {
-    res.render('signup', { data: { error: "Nhập thiếu họ " } });
-  }
-  if (user.firstname.trim().length == 0) // username là name = "username" trong form 
-  {
-    res.render('signup', { data: { error: "Nhập thiếu tên " } });
-  }
-  if (user.email.trim().length == 0) {
-    res.render('signup', { data: { error: "Nhập thiếu email " } });
-  }
-  if (user.password.trim().length < 4) {
-    res.render('signup', { data: { error: "Mật khẩu quá ngắn" } });
-  }
-  if (user.password != user.repassword && user.password.trim().length != 0) {
-    res.render('signup', { data: { error: "Nhập sai" } });
-
-  }
-
-  var _checkE = user_md.checkU(user.email) // check user có trong database không
-  if (_checkE) { //nếu có kết quả
-    _checkE.then(function (data) {   // _check.then để lấy mảng đối tượng trả về là data
-      var _data = data[0]; // gán _data = mảng đối tượng thứ 0 
-      if (data.length == 0 || data == undefined) // nếu mảng đối tượng có độ dài bằng 0 hoặc chưa được định nghĩa (undefined) thì tiến hành nhập user
-      {
-        var date = new Date();
-        var _pass = hash.hash_password(user.password); // hash pass word bằng bcryp
-        var iUser = {
-          First_name: user.firstname,
-          Last_name: user.lastname,
-          Email: user.email,
-          Pass: _pass,
-          SDT:user.phonenumber,
-          QuyenHan: 'user',
-          Update_at: date,
-          Created_at: date,
-          money:100000
+    if (user.lastname.trim().length == 0) // username là name = "username" trong form 
+    {
+      res.render('signup', { data: { error: "Nhập thiếu họ " } });
+    }
+    if (user.firstname.trim().length == 0) // username là name = "username" trong form 
+    {
+      res.render('signup', { data: { error: "Nhập thiếu tên " } });
+    }
+    if (user.email.trim().length == 0) {
+      res.render('signup', { data: { error: "Nhập thiếu email " } });
+    }
+    if (user.password.trim().length < 4) {
+      res.render('signup', { data: { error: "Mật khẩu quá ngắn" } });
+    }
+    if (user.password != user.repassword && user.password.trim().length != 0) {
+      res.render('signup', { data: { error: "Nhập sai" } });
+  
+    }
+  
+    var _checkE = user_md.checkU(user.email) // check user có trong database không
+    if (_checkE) { //nếu có kết quả
+      _checkE.then(function (data) {   // _check.then để lấy mảng đối tượng trả về là data
+        var _data = data[0]; // gán _data = mảng đối tượng thứ 0 
+        if (data.length == 0 || data == undefined) // nếu mảng đối tượng có độ dài bằng 0 hoặc chưa được định nghĩa (undefined) thì tiến hành nhập user
+        {
+          var date = new Date();
+          var _pass = hash.hash_password(user.password); // hash pass word bằng bcryp
+          var iUser = {
+            First_name: user.firstname,
+            Last_name: user.lastname,
+            Email: user.email,
+            Pass: _pass,
+            SDT:user.phonenumber,
+            QuyenHan: 'user',
+            Update_at: date,
+            Created_at: date,
+            money:100000
+          }
+          var addUser = user_md.addUser(iUser); // add user vào databasr
+          if (addUser) { // nếu có dữ liệu user được trả về
+            addUser.then(function (data) {
+  
+              res.render("signup", { data: { q: "đăng ký thành công" } })
+              //  req.session.user=iUser;
+  
+            }).catch(function (error) {
+              res.render('signup', { data: { error: "Đăng ký không thành công " + error } });
+            })
+          }
+        } else if (_data.Email == user.email) { // nếu mảng đối tượng có dữ liệu và mảng đối tượng .Email == user.email thì thông báo tài khoản đã tồn tại
+          res.render('signup', { data: { error: "Tài khoản đã tồn tại" } });
         }
-        var addUser = user_md.addUser(iUser); // add user vào databasr
-        if (addUser) { // nếu có dữ liệu user được trả về
-          addUser.then(function (data) {
-
-            res.render("signup", { data: { q: "đăng ký thành công" } })
-            //  req.session.user=iUser;
-
-          }).catch(function (error) {
-            res.render('signup', { data: { error: "Đăng ký không thành công " + error } });
-          })
-        }
-      } else if (_data.Email == user.email) { // nếu mảng đối tượng có dữ liệu và mảng đối tượng .Email == user.email thì thông báo tài khoản đã tồn tại
-        res.render('signup', { data: { error: "Tài khoản đã tồn tại" } });
-      }
-    }).catch(function (error) { // bắt lỗi error khi CheckE có error
-      res.render('signup', { data: { error: "Đăng ký không thành công " + error } });
-    })
+      }).catch(function (error) { // bắt lỗi error khi CheckE có error
+        res.render('signup', { data: { error: "Đăng ký không thành công " + error } });
+      })
+    }
+  }else{
+    res.redirect(req.originalUrl);
   }
+  
 });
 
 module.exports = router; 
