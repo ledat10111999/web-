e2eea0c83098e29d14fd9b0a6cbf5cd2319bcd19
ledat10111999 <56331527+ledat10111999@users.noreply.github.com>
2020-05-31 23:27:39 +0700

    new

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+function convert(str) {
+  var date = new Date(str),
+    mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+    day = ("0" + date.getDate()).slice(-2);
+  return [ day,mnth,date.getFullYear()].join("-");
+}
 /* GET users listing. */
 router.get('/users/:id', function (req, res, next) {
   var user = req.session.user;
   if(user){
+    user.Created_at = convert(user.Created_at);
     res.render('inforUser',{data:{sign:user}});
   }else{
     res.redirect('/')
